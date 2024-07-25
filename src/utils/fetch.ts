25361297cce7b6bb8b75c5bdcc486889b4754947
Arya Adyatma <88531676+aryadytm@users.noreply.github.com>
2024-07-25 11:44:50 +0700

    Make streaming faster

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -107,7 +107,7 @@ const createSmoothMessage = (params: { onTextUpdate: (delta: string, text: strin
 
   // define startAnimation function to display the text in buffer smooth
   // when you need to start the animation, call this function
-  const startAnimation = (speed = 2) =>
+  const startAnimation = (speed = 999) =>
     new Promise<void>((resolve) => {
       if (isAnimationActive) {
         resolve();
@@ -180,7 +180,7 @@ const createSmoothToolCalls = (params: {
     }
   };
 
-  const startAnimation = (index: number, speed = 2) =>
+  const startAnimation = (index: number, speed = 999) =>
     new Promise<void>((resolve) => {
       if (isAnimationActives[index]) {
         resolve();
@@ -206,7 +206,7 @@ const createSmoothToolCalls = (params: {
             params.onToolCallsUpdate(toolCallsBuffer, [...isAnimationActives]);
           }
 
-          animationTimeoutIds[index] = setTimeout(updateToolCall, 16);
+          animationTimeoutIds[index] = setTimeout(updateToolCall, 1);
         } else {
           isAnimationActives[index] = false;
           animationTimeoutIds[index] = null;
@@ -234,7 +234,7 @@ const createSmoothToolCalls = (params: {
     });
   };
 
-  const startAnimations = async (speed = 2) => {
+  const startAnimations = async (speed = 999) => {
     const pools = toolCallsBuffer.map(async (_, index) => {
       if (outputQueues[index].length > 0 && !isAnimationActives[index]) {
         await startAnimation(index, speed);
@@ -383,11 +383,11 @@ export const fetchSSE = async (url: string, options: RequestInit & FetchSSEOptio
       const observationId = response.headers.get(LOBE_CHAT_OBSERVATION_ID);
 
       if (textController.isTokenRemain()) {
-        await textController.startAnimation(15);
+        await textController.startAnimation(999);
       }
 
       if (toolCallsController.isTokenRemain()) {
-        await toolCallsController.startAnimations(15);
+        await toolCallsController.startAnimations(999);
       }
 
       await options?.onFinish?.(output, { observationId, toolCalls, traceId, type: finishedType });
